fd422135195af8c9688ff2f1fc3b575d85cc9bf6
sakamotodd <skmt_dgr@yahoo.co.jp>
2025-05-14 22:57:55 +0900

    ss

--- src/app/(dashboard)/dashboard/settings/page.tsx
+++ src/app/(dashboard)/dashboard/settings/page.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 export default function Settings() {
   return (
     <main className="mx-8 mt-8 flex flex-col gap-y-10">
-
       <nav className="flex gap-x-8">
         <Link href="/dashboard">サマリー</Link>
         <Link href="/dashboard/settings">設定</Link>
